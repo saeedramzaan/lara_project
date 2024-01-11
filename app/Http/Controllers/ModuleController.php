6c0aa28d4ea30f7d75c08e907eda5e490fc4d39b
Saeed Ramzan <saeedramzaan@gmail.com>
2024-01-11 21:04:11 +0530

    add PostGres DB

--- app/Http/Controllers/ModuleController.php
+++ app/Http/Controllers/ModuleController.php
@@ -24,15 +24,39 @@ class ModuleController extends Controller
      */
     public function create()
     {
-        try {
-            DB::connection()->getPdo();
-        } catch (\Exception $e) {
-            die("Could not connect to the database.  Please check your configuration. error:" . $e );
-        }
+        // try {
+        //     DB::connection()->getPdo();
+        // } catch (\Exception $e) {
+        //     die("Could not connect to the database.  Please check your configuration. error:" . $e );
+        // }
     //     $data = Que::get();
 
     //   //  return  response()->json($users);
     //     return json_encode(array('data' => $data,'count' => 4));
+
+
+// $host = $_ENV['PG_HOST'];
+// $port = $_ENV['PG_PORT'];
+// $db = $_ENV['PG_DB'];
+// $user = $_ENV['PG_USER'];
+// $password = $_ENV['PG_PASSWORD'];
+// $endpoint = $_ENV['PG_ENDPOINT'];
+
+$host = "ep-ancient-hat-81967113-pooler.us-east-1.postgres.vercel-storage.com";
+$port = "5432";
+$db = "verceldb";
+$user = "default";
+$password = "PZceg7UF1MpN";
+$endpoint = "ep-ancient-hat-81967113-pooler.us-east-1.postgres.vercel-storage.com";
+
+$connection_string = "host=" . $host . " port=" . $port . " dbname=" . $db . " user=" . $user . " password=" . $password . " options='endpoint=" . $endpoint . "' sslmode=require";
+
+$dbconn = pg_connect($connection_string);
+
+if (!$dbconn) {
+    die("Connection failed: " . pg_last_error());
+}
+echo "Connected successfully";
     }
 
     /**
