dae20e0954ee0732a6990789cafaed27ebb9a870
Saeed Ramzan <saeedramzaan@gmail.com>
2024-01-11 13:34:01 +0530

    add database

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ModuleController;
 
+ 
+Route::get('/module', [ModuleController::class, 'index']);
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
