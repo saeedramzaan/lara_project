5e595bb36a609a9e139589d0ecad8d43b47c736f
Saeed Ramzan <saeedramzaan@gmail.com>
2024-01-12 00:27:20 +0530

    test

--- app/Http/Controllers/ModuleController.php
+++ app/Http/Controllers/ModuleController.php
@@ -26,7 +26,7 @@ class ModuleController extends Controller
     {
         try {
             DB::connection()->getPdo();
-            $data = DB::table('questions')->get();
+           // $data = DB::table('questions')->get();
         } catch (\Exception $e) {
             die("Could not connect to the database.  Please check your configuration. error:" . $e );
         }
