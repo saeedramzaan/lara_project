248086fdcb3d8ea00cde6ffc882fd6e65f81621e
Saeed Ramzan <saeedramzaan@gmail.com>
2024-01-11 15:05:02 +0530

    check cont

--- app/Http/Controllers/ModuleController.php
+++ app/Http/Controllers/ModuleController.php
@@ -24,7 +24,7 @@ class ModuleController extends Controller
      */
     public function create()
     {
-        $data = Question::get();
+        $data = Que::get();
 
       //  return  response()->json($users);
         return json_encode(array('data' => $data,'count' => 4));
